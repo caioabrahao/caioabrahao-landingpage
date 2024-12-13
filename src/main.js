b52928922a1fea5833935df1b4d5ac83534f34a2
Caio Abrahão <contato.caioabrahao@gmail.com>
2024-12-12 23:47:38 -0300

    added cursor black void

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import gsap from 'gsap';
+import { Observer } from "gsap/Observer";
 
-//gsap.to('.gradient-ball', { scale: 1.2, duration: 5, repeat: -1, yoyo: true, stagger: .7 });
+gsap.registerPlugin(Observer);
+
 
 const tl = gsap.timeline({ repeat: -1,});
 
 tl.to('.blue', { x: -600, duration: 3, ease: "sine.inOut",})
 tl.to('.orange', { x: 600, duration: 3,ease: "sine.inOut",}, '<')
-tl.set('.blue', { zIndex: -3,})
+tl.set('.blue', { zIndex: -4,})
 tl.to('.blue', { x: 0, duration: 5, ease: "sine.inOut",})
 tl.to('.orange', { x: 0, duration: 5, ease: "sine.inOut" }, '<')
 
@@ -16,6 +18,19 @@ gsap.from('.hero h1', {duration: .7, y: -25, opacity: 0, ease: "power1.Out", del
 gsap.from('.hero-text p', {duration: .7, y: -25, opacity: 0, ease: "power1.Out", delay: .5,});
 gsap.from('.header', {duration: 1, y: -100, opacity: 0, ease: "power1.Out", delay: 1,});
 
-//gsap.from('.textfield-container', {duration: 1, y: -25, opacity: 0, ease: "power1.Out", delay: 1.3,});
 gsap.from('.textfield-container', {duration: .5, scale: 0, ease: "bounce", delay: 1.3,});
+
+const follower = document.querySelector(".cursor-black-ball");
+Observer.create({
+    target: window,
+    type: "pointer", 
+    onMove: (e) => {
+        // Animate the follower element to the new mouse position
+        gsap.to(follower, {
+          duration: .7,  // Duration of the animation
+          x: e.x - follower.offsetWidth / 2,  // Center the follower on the mouse
+          y: e.y - follower.offsetHeight / 2,  // Center the follower on the mouse
+        })
+    },
+  });
 
